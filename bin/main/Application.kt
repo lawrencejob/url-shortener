24038032e42a86ea9ddc33ff75c00854edbfae08
Lawrence Job <lawrencejob@live.co.uk>
2025-11-10 23:26:27 +0000

    initialise gradle and ktor project

---
+++ bin/main/Application.kt
@@ -0,0 +1,11 @@
+package com.lawrencejob
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureRouting()
+}
